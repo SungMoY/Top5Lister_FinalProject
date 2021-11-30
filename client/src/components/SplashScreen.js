d59f8b2bd787c3347d5c9804fa05e5f8b26148b9
Sung Mo Yang <sungmo.yang@stonybrook.edu>
2021-11-29 21:02:27 -0500

    working on splash screen, elevation affecting appbanner shadow?

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import { Typography } from '@mui/material';
 import theme from '../theme';
 import { ThemeProvider} from '@mui/material';
+import { Button } from '@mui/material';
 
 export default function SplashScreen() {
     return (
@@ -11,8 +12,11 @@ export default function SplashScreen() {
             sx={{
                 width: "100%",
                 height: "93%",
+                background: 'linear-gradient(to top, #1c2f69, #8c93ca)'
             }}
         >
+            <Box pt={6}></Box>
+
             <Typography align="center" variant="h2" color="common.white">
                 Welcome to
             </Typography>
@@ -20,6 +24,18 @@ export default function SplashScreen() {
             <Typography align="center" variant="h1" color="biglettering">
                 Top 5 Lister
             </Typography>
+
+            <Button variant="contained" color="secondary" align="center">
+                Register
+            </Button>
+
+            <Button variant="contained" color="secondary" align="center">
+                Login
+            </Button>
+
+            <Button variant="contained" color="secondary" align="center">
+                Continue as Guest
+            </Button>
         </Box>
 
         </ThemeProvider>
--- client/src/theme/index.js
+++ client/src/theme/index.js
@@ -5,7 +5,26 @@ const theme = createTheme({
         primary: {
             main:'#000000' //this is black so AccountCircleIcon in appBanner can be black
         },
+        secondary: {
+            main: '#fffec1'
+        },
         biglettering: '#fffec1',
+    },
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                body: {
+                    background: 'linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)',
+                    backgroundRepeat: "no-repeat",
+                    backgroundAttachment: "fixed",
+                }
+            }
+        }
+    },
+    typography: {
+        fontFamily: [
+            'Outfit', 'sans-serif'
+        ].join(',')
     }
 })
 
